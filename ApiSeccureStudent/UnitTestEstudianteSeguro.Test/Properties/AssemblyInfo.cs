c3e8fa9754d7a36d5cf9cb1fdbdd0011b8e7277f
AngelicaIsabelCladeraMelgar <a2019110253@estudiantes.upsa.edu.bo>
2020-09-05 10:28:44 -0400

    Se incluyo el proyecto de UnitTest.

---
+++ ApiSeccureStudent/UnitTestEstudianteSeguro.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestEstudianteSeguro.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestEstudianteSeguro.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d49e29a5-35d2-4958-82a5-4c7aabb6743f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ApiSeccureStudent/UnitTestEstudianteSeguro.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ApiSeccureStudent.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UnitTestEstudianteSeguro.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+
+            //Act
+            var ListaEstudiante = studentsController.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(ListaEstudiante);
+        }
+        //Tambien se puede hacer un usiario de prueba
+        //tambien en el post
+    }
+}
