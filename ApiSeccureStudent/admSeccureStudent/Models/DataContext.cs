aeef3de51aa543f993721abc5f4337134baba3c5
AngelicaIsabelCladeraMelgar <a2019110253@estudiantes.upsa.edu.bo>
2020-09-06 23:05:40 -0400

    Se agregó el proyecto de Administración MVC.

---
+++ ApiSeccureStudent/admSeccureStudent/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace admSeccureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<admSeccureStudent.Models.Student> Students { get; set; }
+    }
+}
